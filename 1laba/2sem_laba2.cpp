fe076028f6a3055d1518bc02ecac979a8630f601
Nomad798jack <103955398+Nomad798jack@users.noreply.github.com>
2024-01-23 20:57:09 +0200

    Add files via upload

---
+++ 1laba/2sem_laba2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "func.hpp"
+
+int main()
+{
+	float x;
+	std::cin >> x;
+	float y;
+	std::cin >> y;
+	float h;
+	std::cin >> h;
+	float p = 0.0;
+	float s = 0.0;
+	mt::Parallelogramm p1(x, y, h, p, s);
+	if (p1.SetHeight(h, y))
+	{
+		p1.Print();
+	}
+
+}
+

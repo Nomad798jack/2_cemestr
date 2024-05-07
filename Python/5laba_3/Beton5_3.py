c0cdfdd65f9c78ebd2b26c02e95574b9afabb8a1
Nomad798jack <103955398+Nomad798jack@users.noreply.github.com>
2024-05-07 20:08:28 +0200

    Update Beton5_3.py

--- Python/5laba_3/Beton5_3.py
+++ Python/5laba_3/Beton5_3.py
@@ -1,13 +1,12 @@
-inputFile = open("input.txt", "r")
+inputFile = open("input3.txt", "r")
 inputList = [i.split() for i in inputFile.readlines()]
 inputFile.close()
 
 inputList.sort(key = lambda kid: kid[2])
-
-theYoungestFile = open("the_youngest.txt", "w")
+theYoungestFile = open("output_youngest3.txt", "w")
 theYoungestFile.write(inputList[0][0] + " " + inputList[0][1] + " " + str(inputList[0][2]))
 theYoungestFile.close()
 
-theOldestFile = open("the_oldest.txt", "w")
+theOldestFile = open("output_oldest3.txt", "w")
 theOldestFile.write(inputList[-1][0] + " " + inputList[-1][1] + " " + str(inputList[-1][2]))
 theOldestFile.close()
